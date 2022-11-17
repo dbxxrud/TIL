ea9b439d61c46cf23ec2de056f9429aab98b7c13
YuKyeong Kim <yukyeong.client@gmail.com>
2022-11-17 17:45:03 +0900

    디폴트인자공부중

--- TIL/FunctionOverloading/main.cpp
+++ TIL/FunctionOverloading/main.cpp
@@ -7,8 +7,17 @@
 
 함슈의 디폴트 인자 : 함수의 인자에 기본값을 설정해주는 기능이다.
 기본값이 설정된 인자는 함수를 호출할때 값을 안넣어줄 경우 기본값으로
-설정이 되고 값을 
+설정이 되고 값을 넣어주면 넣어준 값으로 설정이된다.
+
+단, 함수의 디폴트 인자를 설정할때 항상 가장 오른쪽에 있는 인자부터
+차례로 왼쪽으로 오면서 인자를 설정해야 한다.
 */
+
+void OutPut(int Number1, int Number2 = 200)
+{
+	std::cout << "Number1: " << Number1 << std::endl;
+	std::cout << "Number2: " << Number2 << std::endl;
+}
 
 int Sum(int Number1, int Number2)
 {
@@ -22,6 +31,11 @@ float Sum(float Number1, float Number2)
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	std::cout << Sum(10, 20) << std::endl;
+	std::cout << Sum(50.f, 30.f) << std::endl;
+
+	OutPut(100, 500);
+	OutPut(100);
+
 	return 0;
 }
